47b42b8d4bd9a46fc961fd1d892b597167071b9f
joelyou20 <joelcribby@live.ca>
2021-03-08 22:46:42 -0800

    initialize routing for home, pages, and settings

--- webface/src/app/app.module.ts
+++ webface/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -16,7 +16,12 @@ import { SettingsComponent } from './settings/settings.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'pages', component: PagesComponent},
+      {path: 'settings', component: SettingsComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
